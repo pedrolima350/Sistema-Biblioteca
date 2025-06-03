fe56e05c579aadc99b3343c44b3e3bc84f7b4f6a
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-06-03 20:12:55 -0300

    Commit arrumado

---
+++ Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca
+{
+    internal class Conexao
+    {
+
+    }
+}
